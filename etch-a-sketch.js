d8dcde80944b23b21a493d9fb7ca73e091e68824
jmgcito <joshuamguillen@gmail.com>
2022-06-07 19:56:20 -0700

    Added buttons and a sketch display control div - only rainbow mode is functional

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,12 +1,53 @@
 let gridSize = 16;
-let rangeSlider = document.querySelector("#range-slider");
-rangeSlider.value = gridSize;
-let rangeSliderLabel = document.querySelector("#range-slider-label");
+let colorMode = "black";
+let etchASketchMode = false;
 
+const rangeSlider = document.querySelector("#range-slider");
+rangeSlider.value = gridSize;
+const rangeSliderLabel = document.querySelector("#range-slider-label");
+
+// creates grid based on slider grid size
 rangeSlider.oninput = function () {
   gridSize = this.value;
   createGrid(gridSize);
 };
+
+function toggleMode(tempMode) {
+  if (colorMode == tempMode) {
+    colorMode = "black";
+  } else {
+    colorMode = tempMode;
+  }
+}
+
+const rainbowButton = document.querySelector("#rainbow-button");
+rainbowButton.addEventListener("click", function () {
+  toggleMode("rainbow");
+  rainbowButton.classList.toggle("button-on");
+});
+
+// returns value from 0 to 255
+function randomColorValue() {
+  return Math.floor(Math.random() * 256);
+}
+// returns css text that sets background color to random rgb value
+function randomColor() {
+  let red = randomColorValue();
+  let green = randomColorValue();
+  let blue = randomColorValue();
+  return `background-color: rgb(${red},${green},${blue});`;
+}
+
+// square coloring engine
+function colorSquare(square) {
+  //changes squares next color
+  if (colorMode == "rainbow") {
+    square.style.cssText = randomColor();
+  } else if (colorMode == "shader") {
+  } else {
+    square.style.cssText = "background-color: black";
+  }
+}
 
 // creates a square grid given a size
 function createGrid(gridSize) {
@@ -17,6 +58,7 @@ function createGrid(gridSize) {
     gridContainer.removeChild(gridContainer.lastChild);
   }
 
+  // loop lays out grid of squares
   for (let i = 0; i < gridSize; i++) {
     let tempSquareContainer = document.createElement("div");
     tempSquareContainer.classList.add("square-container");
@@ -25,8 +67,17 @@ function createGrid(gridSize) {
     for (let j = 0; j < gridSize; j++) {
       let tempSquare = document.createElement("div");
       tempSquare.classList.add("square");
-      tempSquare.addEventListener("mouseenter", function () {
-        tempSquare.classList.add("black-background");
+      // colors square when mouse is over square element
+      tempSquare.addEventListener("mouseenter", function (e) {
+        //makes sure mouse is clicked to enable drawing
+        if (e.buttons == 1 && !etchASketchMode) {
+          colorSquare(tempSquare);
+        }
+      });
+      // colors square when mouse button is clicked down
+      // allows for individual squares to be colored
+      tempSquare.addEventListener("mousedown", function () {
+        colorSquare(tempSquare);
       });
       tempSquareContainer.appendChild(tempSquare);
     }
@@ -38,4 +89,5 @@ function createGrid(gridSize) {
   rangeSliderLabel.textContent = `${gridSize} x ${gridSize}`;
 }
 
+// initializes grid
 createGrid(gridSize);
