9ab55d857a1feda61a851aea011e88f045bfa59c
jmgcito <joshuamguillen@gmail.com>
2022-06-08 09:40:37 -0700

    All buttons are functional. Adding shading and lightening modes. Screen can now be cleared.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,6 +2,7 @@ let gridSize = 16;
 let colorMode = "black";
 let etchASketchMode = false;
 
+//handles range slider
 const rangeSlider = document.querySelector("#range-slider");
 rangeSlider.value = gridSize;
 const rangeSliderLabel = document.querySelector("#range-slider-label");
@@ -12,6 +13,17 @@ rangeSlider.oninput = function () {
   createGrid(gridSize);
 };
 
+// if etch-a-sketch mode is on, user can draw without clicking mouse
+const etchCheckbox = document.querySelector("#etchCheckbox");
+etchCheckbox.addEventListener("change", function () {
+  if (this.checked) {
+    etchASketchMode = true;
+  } else {
+    etchASketchMode = false;
+  }
+});
+
+// changes color mode
 function toggleMode(tempMode) {
   if (colorMode == tempMode) {
     colorMode = "black";
@@ -20,10 +32,46 @@ function toggleMode(tempMode) {
   }
 }
 
+// sets css to active button
+function setButtons(mode) {
+  if (mode != "rainbow") {
+    rainbowButton.classList.remove("button-on");
+  } else {
+    rainbowButton.classList.toggle("button-on");
+  }
+  if (mode != "shade") {
+    shadingButton.classList.remove("button-on");
+  } else {
+    shadingButton.classList.toggle("button-on");
+  }
+  if (mode != "lighten") {
+    lightenButton.classList.remove("button-on");
+  } else {
+    lightenButton.classList.toggle("button-on");
+  }
+}
+
 const rainbowButton = document.querySelector("#rainbow-button");
 rainbowButton.addEventListener("click", function () {
   toggleMode("rainbow");
-  rainbowButton.classList.toggle("button-on");
+  setButtons(colorMode);
+});
+
+const shadingButton = document.querySelector("#shading-button");
+shadingButton.addEventListener("click", function () {
+  toggleMode("shade");
+  setButtons(colorMode);
+});
+
+const lightenButton = document.querySelector("#lighten-button");
+lightenButton.addEventListener("click", function () {
+  toggleMode("lighten");
+  setButtons(colorMode);
+});
+
+const clearButton = document.querySelector("#clear-button");
+clearButton.addEventListener("click", function () {
+  createGrid(gridSize);
 });
 
 // returns value from 0 to 255
@@ -35,7 +83,7 @@ function randomColor() {
   let red = randomColorValue();
   let green = randomColorValue();
   let blue = randomColorValue();
-  return `background-color: rgb(${red},${green},${blue});`;
+  return `background-color: rgb(${red},${green},${blue}); filter: brightness(100%);`;
 }
 
 // square coloring engine
@@ -43,9 +91,23 @@ function colorSquare(square) {
   //changes squares next color
   if (colorMode == "rainbow") {
     square.style.cssText = randomColor();
-  } else if (colorMode == "shader") {
+  } else if (colorMode == "shade") {
+    //shade mode
+    if (parseInt(square.style.filter.slice(11, 14)) == 100) {
+      square.style.filter = "brightness(90%)";
+    } else {
+      filterPercent = parseInt(square.style.filter.slice(11, 13));
+      square.style.filter = `brightness(${filterPercent - 10}%)`;
+    }
+  } else if (colorMode == "lighten") {
+    //lighten mode
+    if (parseInt(square.style.filter.slice(11, 14)) == 100) {
+    } else {
+      filterPercent = parseInt(square.style.filter.slice(11, 13));
+      square.style.filter = `brightness(${filterPercent + 10}%)`;
+    }
   } else {
-    square.style.cssText = "background-color: black";
+    square.style.cssText = "filter: brightness(0%)";
   }
 }
 
@@ -67,10 +129,13 @@ function createGrid(gridSize) {
     for (let j = 0; j < gridSize; j++) {
       let tempSquare = document.createElement("div");
       tempSquare.classList.add("square");
+      tempSquare.style.cssText = "filter: brightness(100%)";
       // colors square when mouse is over square element
       tempSquare.addEventListener("mouseenter", function (e) {
         //makes sure mouse is clicked to enable drawing
         if (e.buttons == 1 && !etchASketchMode) {
+          colorSquare(tempSquare);
+        } else if (etchASketchMode) {
           colorSquare(tempSquare);
         }
       });
